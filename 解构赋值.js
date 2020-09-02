c9208866149219c916c0a39aaa65c62a4962e55c
huangqi <768963294@qq.com>
2020-09-02 11:36:23 +0800

    测试

---
+++ 解构赋值.js
@@ -0,0 +1,20 @@
+let name = '张三';
+let age = 18;
+let sex = '男';
+let dog = '旺财';
+let obj = {
+    name,age,sex,
+    eat(){console.log('eat');} ,
+    love:dog
+}
+
+console.log(obj);
+
+
+let obj = {
+    name:'张三',
+    age:18,
+};
+
+let {name,age:num,sex = '女'} = obj;
+console.log();
